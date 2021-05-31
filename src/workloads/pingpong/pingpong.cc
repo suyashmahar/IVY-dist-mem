66901ac507465388989b716e6406dc6b7d996d82
Suyash Mahar <contact@suyashmahar.com>
2021-05-31 09:12:55 -0700

    Update pingpong to use libivy

--- src/workloads/pingpong/pingpong.cc
+++ src/workloads/pingpong/pingpong.cc
@@ -6,79 +6,66 @@
  * @brief  Brief description here
  */
 
-#include "httplib.h"
 #include "common.hh"
+#include "libivy.hh"
 
 #include <iostream>
 #include <thread>
 #include <chrono>
 
-std::string hostnames[] = {"localhost", "localhost"};
-size_t ports[] = {40000, 40001};
-
 bool should_send = false;
+
+using namespace libivy;
+
+constexpr size_t ITERATIONS = 5;
 
 int main(int argc, char *argv[]) {
   using namespace std::chrono_literals;
-  
-  if (argc != 2) {
-    std::cout << "Usage: " << argv[0] << " <nodeId>" << std::endl;
+
+  std::string config_fname;
+  size_t id;
+  if (argc != 3) {
+    std::cout << "Usage: " << argv[0]
+	      << " <path to config file>"
+	      << " <nodeId>"
+	      << std::endl;
     exit(1);
-  }
-
-  auto srv_nodeId = std::stoul(argv[1], nullptr, 10);
-
-  auto srv_hostname = hostnames[srv_nodeId];
-  auto srv_port = ports[srv_nodeId];
-
-  auto cl_nodeId = (srv_nodeId + 1)%2;
-  auto cl_port = ports[cl_nodeId];
-
-  std::cout << "Server, hostname = " << srv_hostname
-	    << ", port = " << srv_port << std::endl;
-  std::cout << "Client port = " << cl_port << std::endl;
-
-  httplib::Server srv;
-  
-  should_send = srv_nodeId == 0;
-  
-  srv.Get("/ping", [&](const httplib::Request &, httplib::Response &res){
-    std::cout << "ping received" << std::endl;
-    res.set_content("", "text/plain");
-    should_send = true;
-  });
-  
-  std::thread([&](){srv.listen(srv_hostname.c_str(), srv_port);}).detach();
-
-  auto client_nm = "localhost:" + std::to_string(cl_port);
-  httplib::Client cl(client_nm.c_str());
-  
-  while (true) {
-    if (should_send) {
-      bool done = false;
-
-      while (!done) {
-	try {
-	  auto res = cl.Get("/ping");
-
-	  if (!res) {
-	    std::string err_val = std::to_string((int)res.error());
-	    throw std::runtime_error(err_val);
-	  } else {
-	    done = true;
-	    should_send = false;
-	  }
-	} catch (std::exception &e) {
-	  std::cout << e.what() << std::endl;
-	  std::cout << "Sleeping for 1s" << std::endl;
-	  std::this_thread::sleep_for(1000ms);
-	}
-	
-      }
-    } else {
-      std::cout << "should_send = false, sleeping for 1s" << std::endl;
-      std::this_thread::sleep_for(1000ms);
+  } else {
+    config_fname = std::string(argv[1]);
+    id = std::strtoul(argv[2], nullptr, 10);
+    if (id > 2) {
+      std::cout << "Wrong node ID" << std::endl;
+      exit(1);
     }
   }
+
+  Ivy ivy(config_fname, id);
+  auto [shm, err] = ivy.get_shm();
+  if (err.has_value()) throw std::runtime_error(err.value());
+  
+  auto arr_ul = reinterpret_cast<uint64_t*>(shm);
+
+  auto [is_manager, im_err] = ivy.is_manager();
+  assert(!im_err.has_value() && "Manager lookup failed");
+  
+  if (is_manager) {
+    arr_ul[0] = arr_ul[1] = 0;
+  } else {
+    auto &my_counter = arr_ul[id];
+
+    auto start_tm = std::chrono::high_resolution_clock::now();
+    while (my_counter < ITERATIONS) {
+      std::cout << "Counter updated to "
+		<< my_counter << std::endl;
+    }
+    auto end_tm = std::chrono::high_resolution_clock::now();
+
+    auto dur = end_tm - start_tm;
+
+    std::cout << "Seconds elapsed = "
+	      << dur.count() << std::endl;
+  }
+  
+  while (1);
 }
 
